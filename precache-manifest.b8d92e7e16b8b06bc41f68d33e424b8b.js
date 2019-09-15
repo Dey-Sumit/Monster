a8cea3ba080485055ab1d9d0e1c5e31453313cdf
unknown <sumax333@gmail.com>
2019-09-15 23:34:23 +0530

    Updates

---
+++ precache-manifest.b8d92e7e16b8b06bc41f68d33e424b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11d23d3f9167faffa5ad7b899e7c74a",
+    "url": "/Monster/index.html"
+  },
+  {
+    "revision": "9a4b9ef910f9177ed063",
+    "url": "/Monster/static/css/main.822c26fd.chunk.css"
+  },
+  {
+    "revision": "f06458aafa24eef3acf2",
+    "url": "/Monster/static/js/2.8ff387bf.chunk.js"
+  },
+  {
+    "revision": "9a4b9ef910f9177ed063",
+    "url": "/Monster/static/js/main.f2f5a80b.chunk.js"
+  },
+  {
+    "revision": "9c605d0a6a909b068c17",
+    "url": "/Monster/static/js/runtime~main.558b6533.js"
+  }
+]);
